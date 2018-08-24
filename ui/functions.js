1f19e95987e1a71d59cdf9b2150aa6323dd56804
forcize <pedro@fusionbreak.com>
2018-08-23 22:49:31 -0400

    Code Indentation & Notes
    Just some indentation and notes :P

--- ui/functions.js
+++ ui/functions.js
@@ -1,43 +1,43 @@
 function GetMessages(callback) {
-    var xhr = new XMLHttpRequest()
-    var url = '/fn/Messages/GetMessages'
-    xhr.open('POST', url, true)
-    xhr.setRequestHeader('Content-type', 'application/json')
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            callback(xhr.responseText)
-        }
+  var xhr = new XMLHttpRequest()
+  var url = '/fn/Messages/GetMessages'
+  xhr.open('POST', url, true)
+  xhr.setRequestHeader('Content-type', 'application/json')
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      callback(xhr.responseText)
     }
-    xhr.send()
+  }
+  xhr.send()
 }
 
 function MessageCreate(entry) {
-    var xhr = new XMLHttpRequest()
-    var url = '/fn/Messages/MessageCreate'
-    xhr.open('POST', url, true)
-    xhr.setRequestHeader('Content-type', 'application/json')
-    var data = JSON.stringify(entry)
-    xhr.send(data)
+  var xhr = new XMLHttpRequest()
+  var url = '/fn/Messages/MessageCreate'
+  xhr.open('POST', url, true)
+  xhr.setRequestHeader('Content-type', 'application/json')
+  var data = JSON.stringify(entry)
+  xhr.send(data)
 }
 
 function DraftCreate(entry) {
-    var xhr = new XMLHttpRequest()
-    var url = '/fn/Posts/DraftCreate'
-    xhr.open('POST', url, true)
-    xhr.setRequestHeader('Content-type', 'application/json')
-    var data = JSON.stringify(entry)
-    xhr.send(data)
+  var xhr = new XMLHttpRequest()
+  var url = '/fn/Posts/DraftCreate'
+  xhr.open('POST', url, true)
+  xhr.setRequestHeader('Content-type', 'application/json')
+  var data = JSON.stringify(entry)
+  xhr.send(data)
 }
 
 function GetDrafts(callback) {
-    var xhr = new XMLHttpRequest()
-    var url = '/fn/Posts/GetDrafts'
-    xhr.open('POST', url, true)
-    xhr.setRequestHeader('Content-type', 'application/json')
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            callback(JSON.parse(xhr.responseText))
-        }
+  var xhr = new XMLHttpRequest()
+  var url = '/fn/Posts/GetDrafts'
+  xhr.open('POST', url, true)
+  xhr.setRequestHeader('Content-type', 'application/json')
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      callback(JSON.parse(xhr.responseText))
     }
-    xhr.send()
+  }
+  xhr.send()
 }
