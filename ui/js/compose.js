5de0f3d540b001c47ae46ef6dc0f84b83b2078d0
Unknown <pedro@fusionbreak.com>
2018-08-31 22:10:20 -0400

    Added getParameterbyName() func for retrieviquery strings from URL

--- ui/js/compose.js
+++ ui/js/compose.js
@@ -19,3 +19,8 @@ document.getElementById("saveDraft").addEventListener("click", function(event) {
 
   document.getElementById('notice').style.display = "block";
 });
+
+function getParameterByName(name) {
+    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+}
