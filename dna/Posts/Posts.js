224e29ad446467d5c3ba8f3a2698de8079e48f66
Unknown <pedro@fusionbreak.com>
2018-09-14 22:44:28 -0400

    Made required changes for adjusting the new Status model and Trash

--- dna/Posts/Posts.js
+++ dna/Posts/Posts.js
@@ -1,11 +1,16 @@
 'use strict';
 
+/* Define Constants */
+var POST_TAG = "post";
+var POST_LINK = "post_link";
+
+/* Public Exposed Functions */
+
 function CreatePost(content) {
-  var post = GetPostType(content.type);
   content.pubdate = new Date();
   content.author = App.Agent.String;
-  var postHash = commit(post.type, content);
-  commit(post.link,{Links:[{Base: App.Agent.Hash,Link: postHash,Tag: post.type}]});
+  var postHash = commit(POST_TAG, content);
+  commit(POST_LINK,{Links:[{Base: App.Agent.Hash,Link: postHash,Tag: POST_TAG}]});
   return postHash;
 }
 
@@ -14,76 +19,79 @@ function GetPost(hash) {
   return post;
 }
 
-function GetPostsByTag(tag) {
-  var links = getLinks(App.Agent.Hash, tag, { Load: true })
+function GetPostsByStatus(status) {
+
+  var links = getLinks(App.Agent.Hash, POST_TAG, { Load: true})
   var posts=[];
+
   links.forEach(function (element){
     var linksObject={};
-    linksObject.hash = element.Hash;
-    linksObject.title = element.Entry.title;
-    linksObject.content = element.Entry.content;
-    linksObject.author = element.Entry.author;
-    linksObject.timestamp = element.Entry.pubdate;
-    posts.push(linksObject);
+    if(element.Entry.status == status){
+      linksObject.hash = element.Hash;
+      linksObject.title = element.Entry.title;
+      linksObject.content = element.Entry.content;
+      linksObject.author = element.Entry.author;
+      linksObject.status = element.Entry.status;
+      linksObject.timestamp = element.Entry.pubdate;
+      posts.push(linksObject);
+    }
   });
 
   return posts;
 }
 
 function DeletePost(post) {
-  var postType = GetPostType(post.type);
-
-  remove(post.hash, post.message)
-
-  commit(postType.link,{
-    Links: [
-      {
-        Base: App.Agent.Hash,
-        Link: post.hash,
-        Tag: postType.type,
-        LinkAction: HC.LinkAction.Del
-      }
-    ]
-  });
-  return post.hash;
+  if(post.status !== "trash"){
+    var oldEntry = GetPost(post.hash);
+    oldEntry.hash = post.hash;
+    oldEntry.status = "trash";
+    oldEntry.message = "Sent to the Trash";
+    oldEntry.lastupdate = new Date();
+    EditPost(oldEntry);
+  }else{
+    commit(POST_LINK,{
+      Links: [
+        {
+          Base: App.Agent.Hash,
+          Link: post.hash,
+          Tag: POST_TAG,
+          LinkAction: HC.LinkAction.Del
+        }
+      ]
+    });
+  }
+  return true;
 }
 
 function EditPost(post) {
 
-  var postType = GetPostType(post.type); //the type who actioned the function
   var oldEntry = GetPost(post.hash);
 
   /* Check if the hash-string is valid before processing */
-  /* TODO: Check if the hash-string contains a proper entry type */
 
   if (oldEntry !== HC.HashNotFound) {
 
-    post.pubdate = new Date();
-    post.author = App.Agent.String;
-
     var hash = update(
-      oldEntry.type, //The original type assigned to the entry
+      POST_TAG,
       {
         title: post.title,
         content: post.content,
-        pupdate: post.pubdate,
-        type: post.type,
-        author: post.author
+        pupdate: oldEntry.pubdate,
+        lastupdate: new Date(),
+        status: post.status,
+        author: oldEntry.author
       },
       post.hash
     );
 
-    oldEntry.hash = post.hash;
-    oldEntry.message = "Deleted"; //TODO: Better Reasons if Transition or Update
+    DeletePost(post);
 
-    DeletePost(oldEntry);
-
-    commit(postType.link, {
+    commit(POST_LINK, {
       Links: [
         {
           Base: App.Agent.Hash,
           Link: hash,
-          Tag: postType.type //new entry or same entry
+          Tag: POST_TAG
         }
       ]
     });
@@ -91,32 +99,6 @@ function EditPost(post) {
   }else{
     return "The hash you have introduced is not a valid!"
   }
-}
-
-// -----------------------------------------------------------------
-//  Helper Functions
-// -----------------------------------------------------------------
-
-/*
-* Create constants for handling the links and commits
-* @param {string} postType - the type of entry, needs to be sent via POST
-* @return {object} that contains the information needed for links
-*/
-
-function GetPostType(postType){
-  var post = {};
-  switch (postType)
-  {
-    case "draft":
-      post.type = "draft";
-      post.link = "draft_link";
-      break;
-    case "publish":
-      post.type = "publish";
-      post.link = "publish_link";
-      break;
-  }
-  return post;
 }
 
 // -----------------------------------------------------------------
@@ -146,10 +128,8 @@ function genesis () {
 */
 function validateCommit (entryName, entry, header, pkg, sources) {
   switch (entryName) {
-    case "draft":
-    case "publish":
-    case "draft_link":
-    case "publish_link":
+    case POST_TAG:
+    case POST_LINK:
     // be sure to consider many edge cases for validating
     // do not just flip this to true without considering what that means
     // the action will ONLY be successfull if this returns true, so watch out!
@@ -172,10 +152,8 @@ function validateCommit (entryName, entry, header, pkg, sources) {
 */
 function validatePut (entryName, entry, header, pkg, sources) {
   switch (entryName) {
-    case "draft":
-    case "publish":
-    case "draft_link":
-    case "publish_link":
+    case POST_TAG:
+    case POST_LINK:
     // be sure to consider many edge cases for validating
     // do not just flip this to true without considering what that means
     // the action will ONLY be successfull if this returns true, so watch out!
@@ -198,10 +176,8 @@ function validatePut (entryName, entry, header, pkg, sources) {
 */
 function validateMod (entryName, entry, header, replaces, pkg, sources) {
   switch (entryName) {
-    case "draft":
-    case "publish":
-    case "draft_link":
-    case "publish_link":
+    case POST_TAG:
+    case POST_LINK:
     // be sure to consider many edge cases for validating
     // do not just flip this to true without considering what that means
     // the action will ONLY be successfull if this returns true, so watch out!
@@ -222,10 +198,8 @@ function validateMod (entryName, entry, header, replaces, pkg, sources) {
 */
 function validateDel (entryName, hash, pkg, sources) {
   switch (entryName) {
-    case "draft":
-    case "publish":
-    case "draft_link":
-    case "publish_link":
+    case POST_TAG:
+    case POST_LINK:
     // be sure to consider many edge cases for validating
     // do not just flip this to true without considering what that means
     // the action will ONLY be successfull if this returns true, so watch out!
@@ -247,10 +221,8 @@ function validateDel (entryName, hash, pkg, sources) {
 */
 function validateLink (entryName, baseHash, links, pkg, sources) {
   switch (entryName) {
-    case "draft":
-    case "publish":
-    case "draft_link":
-    case "publish_link":
+    case POST_TAG:
+    case POST_LINK:
     // be sure to consider many edge cases for validating
     // do not just flip this to true without considering what that means
     // the action will ONLY be successfull if this returns true, so watch out!
