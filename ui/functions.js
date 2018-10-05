8f069c84eee65b88e4291c8d34e8dfa4012acb19
forcize <pedro@fusionbreak.com>
2018-10-04 20:32:47 -0400

    Added new GetPublicPosts Function

--- ui/functions.js
+++ ui/functions.js
@@ -54,6 +54,20 @@ function GetPostsByStatus(entry, callback) {
   xhr.send(data)
 }
 
+function GetPublicPosts(entry, callback) {
+  var xhr = new XMLHttpRequest()
+  var url = '../fn/Posts/GetPublicPosts'
+  xhr.open('POST', url, true)
+  xhr.setRequestHeader('Content-type', 'application/json')
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      callback(JSON.parse(xhr.responseText))
+    }
+  }
+  var data = JSON.stringify(entry)
+  xhr.send(data)
+}
+
 function EditPost(entry, callback) {
   var xhr = new XMLHttpRequest()
   var url = '../fn/Posts/EditPost'
