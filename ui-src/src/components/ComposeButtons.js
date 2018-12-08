294b63b4b5d7ce49b25753801b12b60fe6ea6e05
Pedro Roman <pedro@fusionbreak.com>
2018-12-08 15:12:09 -0400

    Added Conditional Logic for displaying options depending on the status

--- ui-src/src/components/ComposeButtons.js
+++ ui-src/src/components/ComposeButtons.js
@@ -10,21 +10,38 @@ class ComposeButtons extends Component {
       [name]: value
     });
   }
-
   render() {
-    return (
-      <React.Fragment>
-        <div className="form-group button-group float-right m-0">
+    const Buttons = () => {
+      if (this.props.postStatus === 'publish') {
+        return <div className="form-group button-group float-right m-0">
           <button type="button" className="btn btn-blue">PREVIEW</button>
           <button type="button" className="btn btn-red">DELETE</button>
           <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">WARNING: The Hash of the post will be replaced!!</Tooltip>}>
-            <button type="submit" form="submit-content" id="saveDraft" name="status" value="draft" onClick={this.handleChange} className="btn btn-orange mr-1" disabled={!this.props.submitEnabled}>Save as Draft</button>
+            <button type="submit" form="submit-content" id="saveDraft" name="status" value="draft" onClick={this.handleChange} className="btn btn-orange mr-1" disabled={!this.props.submitEnabled}>MARK AS DRAFT</button>
           </OverlayTrigger>
           <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">WARNING: The Hash of the post will be replaced!!</Tooltip>}>
             <button type="submit" form="submit-content" id="publishPost" name="status" value="publish" onClick={this.handleChange} className="btn btn-green" disabled={!this.props.submitEnabled}>UPDATE</button>
           </OverlayTrigger>
-        </div>
-      </React.Fragment>
+        </div>;
+      } else if (this.props.postStatus === 'draft') {
+        return <div className="form-group button-group float-right m-0">
+          <button type="button" className="btn btn-blue">PREVIEW</button>
+          <button type="button" className="btn btn-red">DELETE</button>
+          <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">WARNING: The Hash of the post will be replaced!!</Tooltip>}>
+            <button type="submit" form="submit-content" id="saveDraft" name="status" value="draft" onClick={this.handleChange} className="btn btn-orange mr-1" disabled={!this.props.submitEnabled}>UPDATE DRAFT</button>
+          </OverlayTrigger>
+          <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">WARNING: The Hash of the post will be replaced!!</Tooltip>}>
+            <button type="submit" form="submit-content" id="publishPost" name="status" value="publish" onClick={this.handleChange} className="btn btn-green" disabled={!this.props.submitEnabled}>PUBLISH</button>
+          </OverlayTrigger>
+        </div>;
+      }
+      return <div className="form-group button-group float-right m-0">
+        <button type="submit" form="submit-content" id="saveDraft" name="status" value="draft" onClick={this.handleChange} className="btn btn-orange mr-1" disabled={!this.props.submitEnabled}>SAVE DRAFT</button>
+        <button type="submit" form="submit-content" id="publishPost" name="status" value="publish" onClick={this.handleChange} className="btn btn-green" disabled={!this.props.submitEnabled}>PUBLISH</button>
+      </div>;
+    };
+    return (
+      <Buttons />
     );
   }
 }
